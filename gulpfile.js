03b22906594d22fd96c560817e7c1433a1af9573
Tim Marks <timmarks90@gmail.com>
2019-02-24 10:23:03 -0800

    Added semicolons, removed strict

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,3 @@
-'use strict';
-
 // Required gulp dev dependencies
 const gulp = require('gulp');
 const concat = require('gulp-concat');
@@ -33,24 +31,24 @@ gulp.task('styles', () => {
     .pipe(rename('all.min.css'))
     .pipe(maps.write('./'))
     .pipe(gulp.dest('dist/styles'));
-})
+});
 
 // gulp images to reduce file size of images and copy into dist/content folder
 gulp.task('images', () => {
     return gulp.src('images/*')
     .pipe(img())
     .pipe(gulp.dest('dist/content'));
-})
+});
 
 // watch for changes to any .scss file and run gulp styles when a change is detected to cimpile, concatenate, and minify to dist folder
 gulp.task('watchSass', () => {
     gulp.watch('sass/*.scss', ['styles']);
-})
+});
 
 // delete all of the files and folders in the dist folder
 gulp.task('clean', () => {
     return del(['dist/']);
-})
+});
 
 gulp.task("build", ['clean', 'scripts', 'styles', 'images']);
 
